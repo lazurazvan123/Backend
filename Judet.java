322fc21bfdc9f1589cd4afcd80d675d513a4c2ac
lazurazvan123 <32368440+lazurazvan123@users.noreply.github.com>
2018-03-10 22:05:44 +0200

    Add files via upload

---
+++ Judet.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Judet {
+	public HashMap<String,String> HMOrase;
+	/*Un judet contine un HashMap de orase. O data gasit orasul voi apela functia get 
+	 * a Hashmapului de informatii pentru a stabili daca in locul respectiv se poate sta
+	 * perioada A-B  -------------> Not Done Yet.
+	 */
+	public Judet(HashMap<String, String> hMOrase) {
+		super();
+		HMOrase = hMOrase;
+	}
+}
---
+++ Locations.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Locations {
+	
+	
+	
+	public static HashMap<String,Country> getLocations(String locuri){
+		
+		FileReader fr = null;
+		BufferedReader br = null;
+		String CurrentLine;
+		HashMap<String,Country> HMC = new HashMap<String,Country>();
+
+		try {
+
+			fr = new FileReader(locuri);
+			br = new BufferedReader(fr);
+			
+			while ((CurrentLine = br.readLine()) != null) {
+				String[] arrline = CurrentLine.split(" ");
+				
+				
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+				if (fr != null)
+					fr.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		
+	}
+
+}
